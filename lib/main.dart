9b093451d975ea806559cd967203e3c2852133d1
T3p3s <nightgoblin82@gmail.com>
2025-02-27 18:18:33 +0500

    1

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:lab8/constants/text_styles_value.dart';
 import 'package:lab8/constants/colors.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,18 +22,25 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      localizationsDelegates: context.localizationDelegates,
-      supportedLocales: context.supportedLocales,
-      locale: context.locale,
-      theme: ThemeData(
-        textTheme: TextTheme(
-          displayLarge: AppTextStyles.px12blue,
-          displayMedium: AppTextStyles.px10blue,
-        ),
-      ),
-      home: MyHomePage(),
+    return ScreenUtilInit(
+      designSize: Size(375, 812), 
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          localizationsDelegates: context.localizationDelegates,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
+          theme: ThemeData(
+            textTheme: TextTheme(
+              displayLarge: AppTextStyles.px12blue.copyWith(fontSize: 12.sp),
+              displayMedium: AppTextStyles.px10blue.copyWith(fontSize: 10.sp),
+            ),
+          ),
+          home: MyHomePage(),
+        );
+      },
     );
   }
 }
@@ -50,9 +58,9 @@ class MyHomePage extends StatelessWidget {
           children: [
             Text(
               'hello_text'.tr(),
-              style: AppTextStyles.px12blue,
+              style: AppTextStyles.px12blue.copyWith(fontSize: 14.sp),
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 20.h), 
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -60,14 +68,20 @@ class MyHomePage extends StatelessWidget {
                   onPressed: () {
                     context.setLocale(Locale('en'));
                   },
-                  child: Text('English'),
+                  child: Text(
+                    'English',
+                    style: TextStyle(fontSize: 14.sp),
+                  ),
                 ),
-                SizedBox(width: 10),
+                SizedBox(width: 10.w), 
                 ElevatedButton(
                   onPressed: () {
                     context.setLocale(Locale('ru'));
                   },
-                  child: Text('Русский'),
+                  child: Text(
+                    'Русский',
+                    style: TextStyle(fontSize: 14.sp),
+                  ),
                 ),
               ],
             ),
